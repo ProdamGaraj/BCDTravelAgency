686dbac5e62efd1fe99602b2addf316643ade79e
ProdamGaraj <prodgaraj@yandex.ru>
2023-08-01 00:36:12 +0300

    minor changes

--- dvgKiprBot/src/main/java/сore/DvgKiprBot.java
+++ dvgKiprBot/src/main/java/сore/DvgKiprBot.java
@@ -3,6 +3,7 @@ package сore;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -26,6 +27,7 @@ import java.util.Optional;
 
 @Log4j
 @Singleton
+@SpringBootApplication
 public class DvgKiprBot extends TelegramLongPollingBot {
 
     private final MessageHandler messageHandler;
