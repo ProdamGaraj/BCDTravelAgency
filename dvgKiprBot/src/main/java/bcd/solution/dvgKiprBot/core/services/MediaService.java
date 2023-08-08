bdb6cdb4b27f6dfcfeb2a165c5fcbb144d56026f
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2023-08-08 21:58:15 +0300

    fix images problem

--- dvgKiprBot/src/main/java/bcd/solution/dvgKiprBot/core/services/MediaService.java
+++ dvgKiprBot/src/main/java/bcd/solution/dvgKiprBot/core/services/MediaService.java
@@ -1,6 +1,7 @@
 package bcd.solution.dvgKiprBot.core.services;
 
 import lombok.SneakyThrows;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.media.InputMedia;
@@ -10,7 +11,9 @@ import bcd.solution.dvgKiprBot.core.models.CustomTour;
 import bcd.solution.dvgKiprBot.core.models.Hotel;
 import bcd.solution.dvgKiprBot.core.models.Resort;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Service
 public class MediaService {
@@ -18,22 +21,23 @@ public class MediaService {
     //TODO: absolute path is piece of shit has to be rewrote quickly!
     @SneakyThrows
     public InputFile getStartMessageMedia() {
+        ClassPathResource test = new ClassPathResource("images/kiprstart.jpg");
         InputFile file = new InputFile();
-        file.setMedia(new FileInputStream("src/main/resources/images/kiprstart.jpg"), "kiprstart.jpg");
+        file.setMedia(test.getInputStream(), "kiprstart.jpg");
         return file;
     }
 
     @SneakyThrows
     public InputFile getMediaForActivity(Activity activity){
         InputFile file = new InputFile();
-        file.setMedia(new FileInputStream("src/main/resources/images/images/00.jpg"), "00.jpg");
+        file.setMedia((new ClassPathResource("images/00.jpg")).getInputStream(), "00.jpg");
         //TODO logic
         return file;
     }
     @SneakyThrows
     public InputFile getMediaForCustomTour(CustomTour tour){
         InputFile file = new InputFile();
-        file.setMedia(new FileInputStream("src/main/resources/images//hotels/1187/big/IMG_7174.webp"), "IMG_7174.webp");
+        file.setMedia((new ClassPathResource("images/00.jpg")).getInputStream(), "IMG_7174.webp");
         //TODO logic
         return file;
     }
@@ -53,21 +57,21 @@ public class MediaService {
     @SneakyThrows
     public InputMedia updateMediaForActivity(Activity activity){
         InputMedia file = new InputMediaPhoto();
-        file.setMedia(new FileInputStream("src/main/resources/images/00.jpg"), "00.jpg");
+        file.setMedia((new ClassPathResource("images/00.jpg")).getInputStream(), "00.jpg");
         //TODO logic
         return file;
     }
     @SneakyThrows
     public InputMedia updateMediaForCustomTour(CustomTour tour){
         InputMedia file = new InputMediaPhoto();
-        file.setMedia(new FileInputStream("src/main/resources/images/hotels/1187/big/IMG_7174.webp"), "IMG_7174.webp");
+        file.setMedia((new ClassPathResource("images/00.jpg")).getInputStream(), "IMG_7174.webp");
         //TODO logic
         return file;
     }
     @SneakyThrows
     public InputMedia updateMediaForResort(Resort resort){
         InputMedia file = new InputMediaPhoto();
-        file.setMedia(new FileInputStream("src/main/resources/images/hotels/1158/big/Beach.webp"), "Beach.webp");
+        file.setMedia((new ClassPathResource("images/00.jpg")).getInputStream(), "Beach.webp");
 
         //TODO logic
         return file;
@@ -75,7 +79,7 @@ public class MediaService {
     @SneakyThrows
     public InputMedia updateMediaForHotel(Hotel hotel){
         InputMedia file = new InputMediaPhoto();
-        file.setMedia(new FileInputStream("src/main/resources/images/hotels/1158/big/Double_-Twin_Rooms.webp"), "Double_-Twin_Rooms.webp");
+        file.setMedia((new ClassPathResource("images/00.jpg")).getInputStream(), "Double_-Twin_Rooms.webp");
         //file.setMedia("https://picsum.photos/id/25/200/300");
         //TODO logic
         return file;
@@ -83,7 +87,7 @@ public class MediaService {
     @SneakyThrows
     public InputMedia updateMediaForStart(){
         InputMedia file = new InputMediaPhoto();
-        file.setMedia(new FileInputStream("src/main/resources/images/kiprstart.jpg"), "kiprstart.jpg");
+        file.setMedia((new ClassPathResource("images/kiprstart.jpg")).getInputStream(), "kiprstart.jpg");
         //TODO logic
         return file;
     }
