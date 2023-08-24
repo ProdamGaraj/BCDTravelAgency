5e16140ae1f13159ee2f6e992b0e0df558d678e0
ProdamGaraj <prodgaraj@yandex.ru>
2023-08-24 16:28:28 +0300

    docker update and code cleanup

--- old/dvgKiprBot/src/main/java/core/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package core.repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import core.models.User;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
